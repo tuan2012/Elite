05eb9be43a85ec8a3700742b6853f56866f9d91d
tuan2012 <phampham1109@gmail.com>
2022-04-15 10:11:46 +0700

    init package structure for project

--- src/main/java/com/example/demo/domain/ProductCategory.java
+++ src/main/java/com/example/demo/domain/ProductCategory.java
@@ -6,7 +6,6 @@ import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "")
 public class ProductCategory {
     @Id
     @GeneratedValue(generator = "uuid2")
--- src/main/java/com/example/demo/repository/UserRespository.java
+++ src/main/java/com/example/demo/repository/UserRespository.java
@@ -1,4 +1,4 @@
 package com.example.demo.repository;
 
-public interface ProductRespository {
+public interface UserRespository {
 }
